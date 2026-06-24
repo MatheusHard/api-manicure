8236c28e371f73083c0735588434418c9da177cf
MatheusHard <matheushard2013@gmail.com>
2026-06-24 18:28:19 -0300

    [FIX] Correcao filter jwt

--- src/main/java/com/infotrapichao/api_manicure/src/distributed/interfaces/configuration/jwt/WebSecurityConfig.java
+++ src/main/java/com/infotrapichao/api_manicure/src/distributed/interfaces/configuration/jwt/WebSecurityConfig.java
@@ -42,15 +42,20 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthenticationFilter jwtAuthenticationFilter) throws Exception {
         http
-                .csrf(csrf -> csrf.ignoringRequestMatchers("/login", "/users",  "/gastos/**", "/agendasdepagamento/**", "/dashboard/**"))
+                .csrf(csrf -> csrf.ignoringRequestMatchers("/login", "/users", "/clientes/**", "/agendamentos/**", "/uploads/**"))
                 .cors(Customizer.withDefaults()) // 👈 habilita CORS com configuração default (usa o CorsConfigurationSource abaixo)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(SWAGGER_WHITELIST).permitAll() // 🔹 Libera acesso ao Swagger
-                        .requestMatchers(HttpMethod.POST, "/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/login").permitAll() // acesso público
+                        .requestMatchers(HttpMethod.GET, "/fotos").permitAll() // Libera acesso público
+                        .requestMatchers("/uploads/**").permitAll() // Libera arquivos estáticos se necessário
                         .requestMatchers( "/users").hasAnyRole( "MANAGERS") // apenas os Admins podem chamar
-                        .requestMatchers("/gastos").hasAnyRole( "MANAGERS" , "USERS") // apenas os Admins e Users podem chamar
-                        .requestMatchers("/agendasdepagamento").hasAnyRole( "MANAGERS" , "USERS") // apenas os Admins e Users podem chamar
-                        .requestMatchers("/dashboard").hasAnyRole( "MANAGERS" , "USERS") // apenas os Admins e Users podem chamar
+                        .requestMatchers(HttpMethod.POST,"/clientes").hasAnyRole( "MANAGERS" , "USERS") // apenas os Admins e Users podem chamar
+                        .requestMatchers(HttpMethod.PUT,"/clientes").hasAnyRole( "MANAGERS" , "USERS") // apenas os Admins e Users podem chamar
+                        .requestMatchers(HttpMethod.GET,"/clientes").hasAnyRole( "MANAGERS", "USERS") // apenas os Admins e Users podem chamar
+                        .requestMatchers(HttpMethod.POST,"/agendamentos").hasAnyRole( "MANAGERS", "USERS") // apenas os Admins e Users  podem chamar
+                        .requestMatchers(HttpMethod.PUT,"/agendamentos").hasAnyRole( "MANAGERS", "USERS") // apenas os Admins e Users podem chamar
+                        .requestMatchers(HttpMethod.GET,"/agendamentos").hasAnyRole( "MANAGERS", "USERS") // apenas os Admins e Users podem chamar
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class) // Adiciona o filtro JWT
